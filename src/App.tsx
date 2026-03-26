50807a2dbf597dcf74d84d3a53ee8a66a1e90c65
rcVIKASH <vikash1610m@gmail.com>
2026-03-26 23:16:20 +0530

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,11 @@ import {
   useWallet,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 import {
   WalletModalProvider,
   WalletDisconnectButton,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
